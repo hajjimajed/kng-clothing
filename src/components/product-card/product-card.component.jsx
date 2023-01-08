fab76282c4410756b6b38d026bcf311aeebbe308
hajjimajed <hajjimajed78@gmail.com>
2023-01-08 13:39:31 +0100

    adding cart items and cart count

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -2,9 +2,16 @@ import './product-card.styles.scss'
 
 import Button from '../button/button.component';
 
+import { useContext } from 'react';
+import { CartContext } from '../../contexts/cart.context';
+
 const ProductCard = ({ product }) => {
 
     const { name, price, imageUrl } = product;
+
+    const { addItemToCart } = useContext(CartContext);
+
+    const addProductToCart = () => addItemToCart(product);
 
     return (
         <div className='product-card-container'>
@@ -15,7 +22,7 @@ const ProductCard = ({ product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to cart</Button>
+            <Button buttonType='inverted' onClick={addProductToCart} >Add to cart</Button>
         </div>
     );
 
